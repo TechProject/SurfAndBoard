026568ed84c9460dddd488992e5dbf0fae3cb8e8
techproject <codingusage@gmail.com>
2015-10-12 23:17:41 -0400

    added weather link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/about' => 'pages#about'
+  get '/weather' => 'pages#weather'
   root 'pages#home'
   
   # The priority is based upon order of creation: first created -> highest priority.
